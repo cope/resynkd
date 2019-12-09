21b9001931a6dfa10f56c8ef4ca93ee2da16b8e1
Predrag Stojadinovic <predrag@stojadinovic.net>
2019-12-09 19:55:13 +0100

    wip

--- poc1/client/client.js
+++ poc1/client/client.js
@@ -3,8 +3,26 @@
 const _ = require('lodash');
 const rxjs = require('rxjs');
 const lowdb = require('lowdb');
+const LocalStorage = require('./node_modules/lowdb/adapters/LocalStorage');
+
+const adapter = new LocalStorage('db');
+const db = lowdb(adapter);
+
+db.defaults({
+	posts: [],
+	user: {},
+	count: 1
+}).write();
+
+db.get('posts')
+	.push({id: 3, title: 'lowdb'})
+	.write();
 
 const tmp = require('./src/tmp');
 
 console.log(_.range(6));
 console.log(tmp(6));
+
+// setTimeout(() => {
+// 	window.localStorage.clear();
+// }, 5000);
