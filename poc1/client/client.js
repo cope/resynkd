8dcd60cf6d4f76036f8aba9d7bfe3020fbe0aea8
predrags <predrags@nvidia.com>
2019-12-10 16:29:46 +0100

    wip

--- poc1/client/client.js
+++ poc1/client/client.js
@@ -9,20 +9,37 @@ const adapter = new LocalStorage('db');
 const db = lowdb(adapter);
 
 db.defaults({
-	posts: [],
-	user: {},
-	count: 1
-}).write();
-
-db.get('posts')
-	.push({id: 3, title: 'lowdb'})
+		puppies: [],
+		user: {},
+		count: 1
+	})
 	.write();
+
+// db.get('puppies')
+// 	.push({id: 3, title: 'lowdb'})
+// 	.write();
 
 const tmp = require('./src/tmp');
 
 console.log(_.range(6));
 console.log(tmp(6));
+console.log(rxjs);
 
 // setTimeout(() => {
 // 	window.localStorage.clear();
 // }, 5000);
+
+const dataSubject = new rxjs.Subject();
+const dataWatchA = dataSubject.subscribe(val => console.log('A:', val));
+const dataWatchB = dataSubject.subscribe(val => console.log('B:', val));
+dataSubject.next('Hello');
+
+const timer = rxjs.interval(1000);
+let timerSub = timer.subscribe(val => {
+	console.log(val);
+	dataSubject.next(val);
+});
+setTimeout(() => timerSub.unsubscribe(), 5000);
+// setTimeout(() => timerSub = timer.subscribe(val => console.log(val)), 10000);
+
+
