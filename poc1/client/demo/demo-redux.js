01441ab3890d145f62101fdf70d0ff1215a48e8f
predrags <predrags@nvidia.com>
2019-12-12 15:44:01 +0100

    adding redux

---
+++ poc1/client/demo/demo-redux.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const redux = require('redux');
+const reduxUndo = require('redux-undo');
+
+const {combineReducers, createStore} = redux;
+const {ActionCreators, default: undoable} = reduxUndo;
+// const {applyMiddleware, bindActionCreators, combineReducers, compose, createStore} = redux;
+
+module.exports = function (n) {
+	let counter = (state = 0, action) => {
+		switch (action.type) {
+			case 'INCREMENT':
+				return state + 1;
+			case 'DECREMENT':
+				return state - 1;
+			default:
+				return state;
+		}
+	};
+
+	// let store = createStore(counter); // <- normal store, without undo
+	let store = createStore(combineReducers({counter: undoable(counter)}));	// with undo
+	store.subscribe(() => console.log('redux counter', JSON.stringify(store.getState().counter.present)));
+
+	store.dispatch({type: 'INCREMENT'});
+	store.dispatch(ActionCreators.undo());
+
+	setTimeout(() => store.dispatch({type: 'INCREMENT'}), 1000);
+	setTimeout(() => store.dispatch(ActionCreators.undo()), 1500);
+	setTimeout(() => store.dispatch({type: 'INCREMENT'}), 2000);
+	setTimeout(() => store.dispatch({type: 'INCREMENT'}), 2500);
+	setTimeout(() => store.dispatch({type: 'DECREMENT'}), 3000);
+	setTimeout(() => store.dispatch({type: 'INCREMENT'}), 3500);
+};
--- poc1/client/demo/demo.js
+++ poc1/client/demo/demo.js
@@ -6,14 +6,22 @@ const rxjs = require('rxjs');
 console.log('rxjs', rxjs);
 console.log('range', _.range(6));
 
+const demoRedux = require('./demo-redux');
 const demoObservable = require('./demo-observable');
 const demoSubject = require('./demo-subject');
 const demoWebsocket = require('./demo-websocket');
 const demoWebsocketSubject = require('./demo-websocket-subject');
 
+const demos = [
+	demoRedux,
+	demoObservable,
+	demoSubject,
+	demoWebsocket,
+	demoWebsocketSubject
+];
+
+const delay = 5000;
 module.exports = function (n) {
-	setTimeout(() => demoObservable(), 100);
-	setTimeout(() => demoSubject(), 5000);
-	setTimeout(() => demoWebsocket(), 10000);
-	setTimeout(() => demoWebsocketSubject(), 12000);
+	let cnt = 0;
+	_.each(demos, demo => setTimeout(() => demo(), (delay * cnt++)));
 };
