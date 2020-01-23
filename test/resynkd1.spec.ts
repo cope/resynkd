f7163e280a94b7ca620635e9fccf6ba7d6752219
predrags <predrags@nvidia.com>
2020-01-23 12:21:54 +0100

    tests cleanup

--- test/resynkd1.spec.ts
+++ test/resynkd1.spec.ts
@@ -5,67 +5,63 @@ import {expect} from 'chai';
 import {Subject} from "rxjs";
 import ReSynkd from "../src/resynkd";
 
-describe('ReSynkd exists', () => {
-	it('ReSynkd should exist', () => {
-		expect(ReSynkd).to.exist;
-	});
+describe('ReSynkd tests', () => {
+	it('ReSynkd should exist', () => expect(ReSynkd).to.exist);
 });
 
-describe('ReSynkd1 tests', () => {
-	describe('endPoint1 tests', () => {
-		const resynkd = new ReSynkd();
+describe('ReSynkd Subject tests', () => {
+	const resynkd = new ReSynkd();
 
-		const subject1_1 = new Subject();
-		resynkd.addSubject('subject1_1', subject1_1);
+	const subject1_1 = new Subject();
+	resynkd.addSubject('subject1_1', subject1_1);
 
-		function rs1_from1to2(msg: string) {
-			resynkd.message(msg, rs1_from2to1);
+	function rs1_from1to2(msg: string) {
+		resynkd.message(msg, rs1_from2to1);
+	}
+
+	function rs1_from2to1(msg: string) {
+		resynkd.message(msg, rs1_from1to2);
+	}
+
+	const collect1: number[] = [];
+	const receiver1 = {
+		socketId: 'receiver1',
+		next: (value: any) => {
+			collect1.push(value);
 		}
+	};
 
-		function rs1_from2to1(msg: string) {
-			resynkd.message(msg, rs1_from1to2);
+	resynkd.subscribe({
+		...receiver1,
+		subjectId: 'subject1_1',
+		send: rs1_from2to1,
+		observer: {
+			next: receiver1.next,
+			error: (err) => ({})
 		}
+	});
 
-		const collect1: number[] = [];
-		const receiver1 = {
-			socketId: 'receiver1',
-			next: (value: any) => {
-				collect1.push(value);
-			}
-		};
+	subject1_1.next(1);
+	subject1_1.next(2);
+	subject1_1.error("oops");
 
-		resynkd.subscribe({
+	resynkd.unsubscribe({
+		...receiver1,
+		subjectId: 'subject1_1',
+		send: rs1_from2to1,
+	});
+
+	it('Subject values should be correct', () => {
+		expect(collect1).to.deep.equal([1, 2]);
+		expect(resynkd.message('yei', (msg) => console.log)).to.equal(false);
+	});
+	it('Double subscription should fail', () => {
+		expect(() => resynkd.addSubject('subject1_1', subject1_1)).to.throw;
+		expect(() => resynkd.subscribe({
 			...receiver1,
 			subjectId: 'subject1_1',
 			send: rs1_from2to1,
-			observer: {
-				next: receiver1.next,
-				error: (err) => ({})
-			}
-		});
-
-		subject1_1.next(1);
-		subject1_1.next(2);
-		subject1_1.error("oops");
-
-		resynkd.unsubscribe({
-			...receiver1,
-			subjectId: 'subject1_1',
-			send: rs1_from2to1,
-		});
-
-		it('Values should be correct', () => {
-			expect(collect1).to.deep.equal([1, 2]);
-			expect(resynkd.message('yei', (msg) => console.log)).to.equal(false);
-		});
-		it('Double subscription should fail', () => {
-			expect(() => resynkd.addSubject('subject1_1', subject1_1)).to.throw;
-			expect(() => resynkd.subscribe({
-				...receiver1,
-				subjectId: 'subject1_1',
-				send: rs1_from2to1,
-				observer: {next: receiver1.next}
-			})).to.throw;
-		});
+			observer: {next: receiver1.next}
+		})).to.throw;
 	});
 });
--- test/resynkd2.spec.ts
+++ test/resynkd2.spec.ts
@@ -5,51 +5,49 @@ import {expect} from 'chai';
 import {BehaviorSubject} from "rxjs";
 import ReSynkd from "../src/resynkd";
 
-describe('ReSynkd2 tests', () => {
-	describe('endPoint2 tests', () => {
-		const resynkd = new ReSynkd();
+describe('ReSynkd BehaviorSubject tests', () => {
+	const resynkd = new ReSynkd();
 
-		const subject2_2 = new BehaviorSubject(0);
-		resynkd.addSubject('subject2_2', subject2_2);
+	const subject2_2 = new BehaviorSubject(0);
+	resynkd.addSubject('subject2_2', subject2_2);
 
-		function rs2_from1to2(msg: string) {
-			resynkd.message(msg, rs2_from2to1);
+	function rs2_from1to2(msg: string) {
+		resynkd.message(msg, rs2_from2to1);
+	}
+
+	function rs2_from2to1(msg: string) {
+		resynkd.message(msg, rs2_from1to2);
+	}
+
+	const collect2: number[] = [];
+	const receiver2 = {
+		socketId: 'receiver2',
+		next: (value: any) => {
+			collect2.push(value);
 		}
+	};
 
-		function rs2_from2to1(msg: string) {
-			resynkd.message(msg, rs2_from1to2);
+	resynkd.subscribe({
+		...receiver2,
+		subjectId: 'subject2_2',
+		send: rs2_from2to1,
+		observer: {
+			next: receiver2.next,
+			complete: () => ({})
 		}
+	});
 
-		const collect2: number[] = [];
-		const receiver2 = {
-			socketId: 'receiver2',
-			next: (value: any) => {
-				collect2.push(value);
-			}
-		};
+	subject2_2.next(1);
+	subject2_2.next(2);
+	subject2_2.complete();
 
-		resynkd.subscribe({
-			...receiver2,
-			subjectId: 'subject2_2',
-			send: rs2_from2to1,
-			observer: {
-				next: receiver2.next,
-				complete: () => ({})
-			}
-		});
+	resynkd.unsubscribe({
+		...receiver2,
+		subjectId: 'subject2_2',
+		send: rs2_from2to1,
+	});
 
-		subject2_2.next(1);
-		subject2_2.next(2);
-		subject2_2.complete();
-
-		resynkd.unsubscribe({
-			...receiver2,
-			subjectId: 'subject2_2',
-			send: rs2_from2to1,
-		});
-
-		it('Values should be correct', () => {
-			expect(collect2).to.deep.equal([0, 1, 2]);
-		});
+	it('BehaviorSubject values should be correct', () => {
+		expect(collect2).to.deep.equal([0, 1, 2]);
 	});
 });
